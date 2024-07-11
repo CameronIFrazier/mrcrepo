a4b9af22461b2caaddc4652ad462d18b693cfe3a
CameronIFrazier <taterfrazier1@gmail.com>
2024-07-10 18:11:07 -0700

    need functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import rhscalc from "./images/illustration-empty.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="centerdualbox">
+          <div className="leftBoxContainer"> 
+          <div className="mortgagecalcdiv">
+            Mortgage Calculator
+            <div className="clearallbutton">Clear All</div>
+          </div>
+          <div className="mortgageamountdiv">
+            <header className="mortgageamountheader">Mortgage Amount</header>
+            <textarea className="mortgageamounttextarea"></textarea>
+          </div>
+          <div className="mortgageinfodiv">
+            <header className="mortagetermheader"> Mortgage Term</header>
+            <header className="interestrateheader"> Interest Rate</header>
+            <textarea></textarea>
+            <textarea className="mortgageAndInterestRateTextAreas"></textarea>
+          </div>
+          <div className="mortgagetypeinfodiv">
+            <header className="mortgagetypeheader"> Mortgage Type</header>
+            <button className="mtypebuttons"> Repayment</button>
+            <button className="mtypebuttons"> Interest Only</button>
+          </div>
+          <div className="calculatePaymentButtoncContainer">
+          <button className="calcpaymentbutton"> Calculate Payments</button>
+          </div>
+          </div>
+          <div className="resultscontainer">
+          <img src={rhscalc} alt="" className="rhscalc" />
+          </div>
+        </div>
+        
+      </div>
+    </>
   );
 }
 
